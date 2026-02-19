2ea09f60d9371f3816df856bb4b0dd7dc2f54099
BogdanStanley <bogdanstanley@cmail.carleton.ca>
2026-02-19 16:17:11 -0500

    added user registration and login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path('', views.index, name='index'),
+    path('profile/', views.user_profile, name='profile'),
 ]
