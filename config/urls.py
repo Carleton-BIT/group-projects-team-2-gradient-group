3995121b49f9d156f85318e75827964c4a837e61
BogdanStanley <bogdanstanley@cmail.carleton.ca>
2026-02-21 14:08:16 -0500

    updated CSS to be Carleton Themed

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from config.views import SignUpView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
