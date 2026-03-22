4fbc5c6a796811e071233b0742b9226acef583b7
BogdanStanley <bogdanstanley@cmail.carleton.ca>
2026-03-22 18:31:13 -0400

    Added filtering and saving

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("product/<int:pk>/", views.product_detail, name="product_detail"),
     path("profile/edit/", views.edit_profile, name="edit_profile"),
     path("my-listings/", views.my_listings, name="my_listings"),
+    path("listings/", views.all_listings, name="all_listings"),
+    path("my-listings/", views.my_listings, name="my_listings"),
+    path("products/<int:pk>/delete/", views.delete_product, name="delete_product"),
+    path("saved-items/", views.saved_items, name="saved_items"),
+    path("products/<int:pk>/save/", views.toggle_save_product, name="toggle_save_product"),
 ]
